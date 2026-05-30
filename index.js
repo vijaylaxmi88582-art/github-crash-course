02263e6cce8d081ff51915fbf8f81b2c7c3682f6
Vijay Laxmi <vijaylaxmi88582@gmail.com>
2026-05-30 09:26:59 +0530

    Added is Above18 in

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const age =24;
-const name = 'vijaylaxmi'
+const name1 = 'vijaylaxmi'
 const dob ='01/01/2003'
 function sayName(){
     console.log('My Name is ' + name)
+}
+function isAbove18(){
+    return age >=18
 }
