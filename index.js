a4e7226d2fd025957f148aeed59cf6fc5f77753f
Vijay Laxmi <vijaylaxmi88582@gmail.com>
2026-05-30 16:35:09 +0530

    commit-1

--- index.js
+++ index.js
@@ -15,4 +15,12 @@ function newFeatureAddB(){
     console.log('This is a new feat')
     console.log('This is a new feat B')
 }
+function newFeatureAddC(){
+    console.log('This is a new feat')
+    console.log('This is a new feat C')
+}
+
+
+
+
 
