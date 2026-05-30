492780f6692e75311f9a59c3306ed23c5f1fcf4a
Vijay Laxmi <vijaylaxmi88582@gmail.com>
2026-05-30 09:39:26 +0530

    Remove18 function

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const age =24;
 const name1 = 'vijaylaxmi'
-const dob ='01/01/2003'
+
 function sayName(){
-    console.log('My Name is ' + name)
-}
-function isAbove18(){
-    return age >=18
+    console.log('My Name is' +name1)
 }
